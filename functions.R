d5f2d65f333ddee479def61c7475231434819351
pawel-wieczynski <94794405+pawel-wieczynski@users.noreply.github.com>
2023-05-08 21:27:32 +0200

    fix open bid function

--- functions.R
+++ functions.R
@@ -90,33 +90,33 @@ open_bid = function(hand){
   hcp = calculate_hcp(hand)
   suits = count_suits(hand)
   
-  if (suits$spades >= 5 & hcp >= 12 & hcp <= 18) {
+  if (suits['spades'] >= 5 & hcp >= 12 & hcp <= 18) {
     return('1 spades')
-  } else if (suits$hearts >= 5 & hcp >= 12 & hcp <= 18) {
+  } else if (suits['hearts'] >= 5 & hcp >= 12 & hcp <= 18) {
     return('1 hearts')
-  } else if (suits$diamonds >= 5 & hcp >= 12 & hcp <= 18) {
+  } else if (suits['diamonds'] >= 5 & hcp >= 12 & hcp <= 18) {
     return('1 diamonds')
-  } else if (suits$clubs >= 2 & hcp >= 12 & hcp <= 18) {
+  } else if (suits['clubs'] >= 2 & hcp >= 12 & hcp <= 18) {
     return('1 clubs')
-  } else if (suits$spades >= 2 & suits$spades <= 4 & 
-             suits$hearts >= 2 & suits$hearts <= 4 & 
-             suits$diamonds >= 2 & suits$diamonds <= 4 &
-             suits$clubs >= 2 & suits$clubs <= 4 &
+  } else if (suits['spades'] >= 2 & suits['spades'] <= 4 & 
+             suits['hearts'] >= 2 & suits['hearts'] <= 4 & 
+             suits['diamonds'] >= 2 & suits['diamonds'] <= 4 &
+             suits['clubs'] >= 2 & suits['clubs'] <= 4 &
              hcp >= 16 & hcp <= 18) {
     return('1 NT')
     
-  } else if (suits$spades >= 5 & hcp >= 19 & hcp <= 22) {
+  } else if (suits['spades'] >= 5 & hcp >= 19 & hcp <= 22) {
     return('2 spades')
-  } else if (suits$hearts >= 5 & hcp >= 19 & hcp <= 22) {
+  } else if (suits['hearts'] >= 5 & hcp >= 19 & hcp <= 22) {
     return('2 hearts')
-  } else if (suits$diamonds >= 4 & hcp >= 19 & hcp <= 22) {
+  } else if (suits['diamonds'] >= 4 & hcp >= 19 & hcp <= 22) {
     return('2 diamonds')
-  } else if (suits$clubs >= 4 & hcp >= 19 & hcp <= 22) {
+  } else if (suits['clubs'] >= 4 & hcp >= 19 & hcp <= 22) {
     return('2 clubs')
-  } else if (suits$spades >= 2 & suits$spades <= 4 & 
-             suits$hearts >= 2 & suits$hearts <= 4 & 
-             suits$diamonds >= 2 & suits$diamonds <= 4 &
-             suits$clubs >= 2 & suits$clubs <= 4 &
+  } else if (suits['spades'] >= 2 & suits['spades'] <= 4 & 
+             suits['hearts'] >= 2 & suits['hearts'] <= 4 & 
+             suits['diamonds'] >= 2 & suits['diamonds'] <= 4 &
+             suits['clubs'] >= 2 & suits['clubs'] <= 4 &
              hcp >= 19 & hcp <= 22) {
     return('2 NT')
   } else {
